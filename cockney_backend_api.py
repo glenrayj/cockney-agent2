9bdf74554cc07f08e07252a0ab1853611cf36cb5
glenrayj <glen@emotionstudios.com>
2025-08-06 15:42:41 -0700

    Add files via upload

---
+++ cockney_backend_api.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+# Load the slang dictionary
+with open("cockney_slang.json", "r") as f:
+    cockney_dict = json.load(f)
+
+@app.route("/api/slang")
+def get_slang():
+    return jsonify(cockney_dict)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
